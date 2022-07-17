0bbd38430c932aed1e9a7fec42a3d3c7e5d4be2b
ico2k2developer <48391330+ico2k2developer@users.noreply.github.com>
2022-07-17 11:55:45 +0200

    Clean up

--- boolean.h
+++ boolean.h
@@ -1,5 +1,5 @@
-#ifndef BOOLEAN_BOOLEAN_H
-#define BOOLEAN_BOOLEAN_H
+#ifndef BOOLEAN_H
+#define BOOLEAN_H
 
 #include <stdint.h>
 
@@ -8,8 +8,8 @@ typedef uint8_t bool_t;
 #define BOOL_MAX_STORAGE    (sizeof(bool_t) << 3)
 
 uint8_t bool_getValue(bool_t boolean, uint8_t index);
-bool_t bool_setValue(bool_t* boolean, uint8_t index, uint8_t value);
-bool_t bool_invertValue(bool_t* boolean,uint8_t index);
-bool_t bool_setAllTo(bool_t* boolean, uint8_t value);
+bool_t  bool_setValue(bool_t* boolean, uint8_t index, uint8_t value);
+bool_t  bool_invertValue(bool_t* boolean,uint8_t index);
+bool_t  bool_setAllTo(bool_t* boolean, uint8_t value);
 
-#endif //BOOLEAN_BOOLEAN_H
+#endif //BOOLEAN_H
--- ctest/boolean.c
+++ ctest/boolean.c
@@ -11,15 +11,9 @@ cteststart()
     assert(bool_setAllTo(&b,1) == b);
     assert(b == UINT8_MAX);
     for(i = 0; i < 16; i++)
-    {
         one(bool_getValue(b,i));
-    }
     for(i = 0; i < 16; i++)
-    {
-        printf("i is %zu, value is %llu, bool is %d\t",i,i&1,b);
         assert(bool_setValue(&b,i,i&1) == b);
-        printf("then is %d\n",b);
-    }
     assert(b == 170);
     assert(bool_invertValue(&b,1) == 168);
 ctestend
