d5cacdcdeee01ca9ec011818478b39e00d58eba9
ico2k2developer <48391330+ico2k2developer@users.noreply.github.com>
2022-07-17 14:09:33 +0200

    Released version 1.0.0

--- release.bat
+++ release.bat
@@ -1,13 +1,9 @@
 @echo off
 mkdir release
 cd release
-mkdir amd64
-mkdir x86
-cd x86
-mklink boolean.h "..\..\boolean.h"
-mklink boolean.dll "..\..\cmake-build-minsizerel-x86\BooleanDll.dll"
-mklink boolean.lib "..\..\cmake-build-minsizerel-x86\BooleanLib.lib"
-cd ..\amd64
-mklink boolean.h "..\..\boolean.h"
-mklink boolean.dll "..\..\cmake-build-minsizerel-amd64\BooleanDll.dll"
-mklink boolean.lib "..\..\cmake-build-minsizerel-amd64\BooleanLib.lib"
+mklink /H "boolean.h" "..\boolean.h"
+mklink /H "boolean x86.dll" "..\cmake-build-minsizerel-x86\BooleanDll.dll"
+mklink /H "boolean x86.lib" "..\cmake-build-minsizerel-x86\BooleanLib.lib"
+mklink /H "boolean amd64.dll" "..\cmake-build-minsizerel-amd64\BooleanDll.dll"
+mklink /H "boolean amd64.lib" "..\cmake-build-minsizerel-amd64\BooleanLib.lib"
+cd ..
